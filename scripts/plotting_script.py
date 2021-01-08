ace71dd3782df86188f0eae58c8056d114e23b10
Andrew Evans <evans908@umn.edu>
2021-01-08 14:03:01 -0600

    a brief plotting script based of the elmonk tutorial notebook

---
+++ scripts/plotting_script.py
@@ -0,0 +1,45 @@
+from elmonk.common.setuptools import data_path, download, elmonk_basepath
+import pandas as pd
+
+dstPath = data_path / 'dst.merged/2018'
+if not dstPath.exists():
+    download('dst_fed636_2018.tuto.tar.gz')
+if not (data_path / 'oms_db.light.2018.hdf5').exists():
+    download('oms_db.light.2018.hdf5')
+import os
+
+os.chdir(elmonk_basepath)
+
+from elmonk.common import HdfLaser
+
+data = HdfLaser('/panfs/roc/groups/4/rusack/evans908/FAIR/ECAL_RADDAM_Data/2016/dst.w447.hdf5')
+print(data.info.columns)
+
+from ecalic import geom
+
+xtals = data.xtal_idx('FED == 636')
+
+iovs = data.iov_idx(['2016-08-01','2016-08-10'])
+
+histories = data.xtal_history(iov_idx=iovs,xtal_idx=xtals)
+
+import numpy as np
+
+xtals_to_plot = np.random.choice(histories.columns,6)
+
+import ecalic.cmsStyle
+import matplotlib.pyplot as plt
+
+ax = histories.plot(y=xtals_to_plot,linestyle='none',marker='.', figsize=[10,4])
+ax.set_ylim(0.80,1.05)
+ax.set_title('Histories')
+ax.legend(ncol=2)
+print(ax)
+
+import elmonk.common.stats as stats
+stat_FED636 = data.xtal_history(iov_idx=iovs, xtal_idx=xtals, func=stats.history_quantiles)
+
+import elmonk.common.plotting as plotting
+plotting.brazilian_flag(pd.DataFrame(stat_FED636),{0:'This is the title for FED636'})
+
+plt.savefig("laser.png")
