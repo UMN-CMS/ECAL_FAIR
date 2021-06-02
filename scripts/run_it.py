9841dd02bcf3869fd1d666f3c15a4be80e3481d4
Andrew Evans <evans908@umn.edu>
2021-06-02 10:27:03 -0500

    arg parsing update

--- scripts/run_it.py
+++ scripts/run_it.py
@@ -1,17 +1,37 @@
+#!/usr/bin/env python3
+
 import df_builder as dfb
 import pandas as pd
 
+import argparse
+############################################################################
+#ARGS
+parser = argparse.ArgumentParser(description='A quick script that interfaces with functions in df_builder to extract dataframes from the raw data')
 
-year = '2018'
-yr = '18'
+parser.add_argument('--year', type=str, choices=['2016','2017','2018'], help='pick a year: 2016,2017,2018', required=True)
+parser.add_argument('--xtal_slice', type=str, help='A string passed to the ecalic.geom dataframe to grab crystal indices, like: ieta==66', required=True)
+parser.add_argument('--out_file', type=str, help='Name of the dataframe you will be writing out, like: ../dataframes/my_df.csv', required=True)
+parser.add_argument('--max_xtals', type=int, help='Max number of crystals to extract', default=10000,required=False)
+
+args = parser.parse_args()
+###########################################################################################################################
+#INPUTS
+
+year = args.year
+yr = args.year[2:]
+
+xtal_sel = args.xtal_slice
+out_file = args.out_file
+max_xtals = args.max_xtals
+
 #this returns a multi-index dataframe, and the xtals that make it up. The first layer of index corresponds to the xtals returned
 dst_dfs, xtals = dfb.pullSlimAndSkim('../ECAL_RADDAM_Data/'+year+'/dst.w447.hdf5',
                                      '../ECAL_RADDAM_Data/'+year+'/oms.hdf5', 
                                      out_file=None, 
                                      slim=False, 
-                                     max_xtals=2,
+                                     max_xtals=max_xtals,
                                      #xtal_sel='(eta_module == 4) & (phi_module == 1) & (LME == 38) & (PNA == 4)'
-                                     xtal_sel='ieta==66'
+                                     xtal_sel=xtal_sel
                                      )
 #load lumi_information
 print('Loading lumi by month: ')
@@ -29,4 +49,4 @@ print('building final df')
 #now we concatenate the dfs
 final_df = pd.concat(comb_dfs, keys=xtals, axis=0)
 #save it out
-final_df.to_csv('slimmed_df_ieta66_'+year+'.csv', index=True)
+final_df.to_csv(out_file, index=True)
